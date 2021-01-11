be3836f3648c9fca7a19db116c52c2de8f525517
getnamo <getnamo@gmail.com>
2021-01-11 14:29:29 -0800

    scaffold receiver

---
+++ Source/GlobalEventSystem/Public/GESBaseReceiverComponent.h
@@ -0,0 +1,15 @@
+// Copyright 2019-current Getnamo. All Rights Reserved
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "GESBaseReceiverComponent.generated.h"
+
+UCLASS(BlueprintType, ClassGroup = "Utility", meta = (BlueprintSpawnableComponent))
+class GLOBALEVENTSYSTEM_API UGESBaseReceiverComponent : public UActorComponent
+{
+	GENERATED_UCLASS_BODY()
+public:
+
+protected:
+};
