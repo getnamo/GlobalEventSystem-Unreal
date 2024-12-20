cee07215d12d0fc15c7957defcd67f5c817fccbd
getnamo <getnamo@gmail.com>
2024-12-19 17:39:52 -0800

    object receiver variant for js

--- Content/Scripts/utility/gesWrapper.js
+++ Content/Scripts/utility/gesWrapper.js
@@ -19,6 +19,10 @@ class GESJsReceiver extends JsOwner.ClassMap['GESJsReceiverBpActor']{
 		this.callbacks[uniqueId](property);
 	}
 
+	OnJsReceiveObj(uniqueId, property){
+		this.callbacks[uniqueId](property);
+	}
+
 	//returns a unique id for potential unbinding
 	bind(domain='global.default', event, callback){
 		//const uniqueKey = domain + '.' + event;
@@ -35,6 +39,30 @@ class GESJsReceiver extends JsOwner.ClassMap['GESJsReceiverBpActor']{
 			uniqueFunctionId);
 		return uniqueFunctionId;
 	}
+
+	bindToObjCallback(domain='global.default', event, callback, uniqueReceiver = false){
+		//const uniqueKey = domain + '.' + event;
+		const uniqueFunctionId = this.NextUniqueReceiverObj()['NextUniqueFunction'];
+
+		if(uniqueReceiver && uniqueFunctionId == 'OnJsReceiveOneParamObj_1'){
+			console.warn('GESJsReceiver: Maximum obj receivers (1) reached (uniqueReceiver: true)!');
+			return '';
+		}
+		
+		if(uniqueFunctionId == 'OnJsReceiveOneParamObj_4'){
+			console.warn('GESJsReceiver: Maximum obj receivers reached!');
+		}
+
+		this.callbacks[uniqueFunctionId] = callback;
+
+		console.log(`UniqueId is <${uniqueFunctionId}>`)
+
+		this.JsGESBindEvent(domain,
+			event,
+			uniqueFunctionId);
+		return uniqueFunctionId;
+	}
+
 	emit(domain='global.default', event, data='', pinned=false){
 		if(typeof data === 'string'){
 			this.JsGESEmitEventOneParamString(domain, event, data, pinned);
